7a4c6a82ff996745123401c93bc31a48dac6059d
fpikus <39199330+fpikus@users.noreply.github.com>
2023-01-14 14:43:11 -0800

    Add files via upload

--- Chapter06/06_c_sort.C
+++ Chapter06/06_c_sort.C
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <iostream>
 
 int less(const void* a, const void* b) {
     return *(const int*)a - *(const int*)b;
@@ -6,5 +7,7 @@ int less(const void* a, const void* b) {
 
 int main() {
     int a[10] = { 1, 10, 2, 9, 3, 8, 4, 7, 5, 0 };
-    qsort(a, sizeof(int), 10, less);
+    qsort(a, 10, sizeof(int), less);
+    for (int x : a) std::cout << x << " ";
+    std::cout << std::endl;
 }
